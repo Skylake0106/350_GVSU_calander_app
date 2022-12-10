a737de6ac47160f83551b9c8dd60d9f82655a7c5
aSabene <94251267+aSabene@users.noreply.github.com>
2022-12-10 16:49:20 -0500

    new syntax for Oracle req -abs

--- faculty-scheduler/pages/api/listDepths.js
+++ faculty-scheduler/pages/api/listDepths.js
@@ -1,18 +1,10 @@
-import { connectToDatabase } from "../../lib/mongodb";
-
-export default async function handler(request, response) {
-    
-    // Connect to MongoDB
-    const { database } = await connectToDatabase();
-    const depths = database.collection("Depth");
-    const professors = database.collection("Professors")
-
-    // Get all of the professors from MongoDB
-    // insert any query parameters into the find() function
-    //find depths by professor and return for the professors page, currently grabs all classes where Zachary Kurmas has a depth of 1
-    const results = await depths.find({Kurmas: 1})
-    .toArray();
-
-    // Return the array of professors as JSON
-    response.status(200).json(results);
-}
+export default async (req, res) => {
+    try {
+      const response = await axios.get('https://apex.oracle.com/pls/apex/facultyschedulerasst/depthchart/cis/');
+      const data = response.data.items;
+  
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
