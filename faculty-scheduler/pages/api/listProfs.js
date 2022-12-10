93740d46db809a87db184d5fde5b12cde77fae26
aSabene <94251267+aSabene@users.noreply.github.com>
2022-12-10 16:48:45 -0500

    new syntax for Oracle req - abs

--- faculty-scheduler/pages/api/listProfs.js
+++ faculty-scheduler/pages/api/listProfs.js
@@ -1,17 +1,10 @@
-import { connectToDatabase } from "../../lib/mongodb";
-
-export default async function handler(request, response) {
-    
-    // Connect to MongoDB
-    const { database } = await connectToDatabase();
-    const collection = database.collection("Professors");
-
-    // Get all of the professors from MongoDB
-    // insert any query parameters into the find() function
-    const results = await collection.find({})
-    .toArray();
-    
-    // Return the array of professors as JSON
-    response.status(200).json(results);
-
-}
+export default async (req, res) => {
+    try {
+      const response = await axios.get('https://apex.oracle.com/pls/apex/facultyschedulerasst/proftable/cis/');
+      const data = response.data.items;
+  
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
